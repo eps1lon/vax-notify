bc54c4d323a51b098d9b38f0adc32b4613e2f679
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2021-04-05 19:19:57 +0200

    feat: Mail new dates (#10)

--- scripts/free-dates.js
+++ scripts/free-dates.js
@@ -1,12 +1,58 @@
 //@ts-check
 import { Octokit } from "@octokit/rest";
-import fetch from "cross-fetch";
+import fetch, { Request } from "cross-fetch";
 import * as fs from "fs/promises";
 import * as path from "path";
 import * as playwright from "playwright";
 import { URL } from "url";
 
+/**
+ * @typedef {object} Mail
+ * @property {(centre: string) => Promise<void>} sendFreeDates
+ */
+
+const __DEV__ = process.env.NODE_ENV !== "production";
 const ISSUE_NUMBER = 8;
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+
+/**
+ * @type {Mail}
+ */
+const dryMail = {
+  async sendFreeDates(centre) {
+    console.log(`free dates for centre '${centre}'`);
+  },
+};
+
+const SENDER_ID = 1499870;
+const LIST_ID = __DEV__
+  ? "9d668146-89c7-47b4-b600-65e1d73ce60f"
+  : "a2915f4d-f1af-4968-80a0-54d802ff578a";
+/**
+ * Mapping from a centre name to a SendGrid suppression_group_id
+ * @type {Record<string, number>}
+ */
+const CENTRE_TO_SUPRESSION_GROUP_ID = {
+  "Bautzen IZ": 22686,
+  "Belgern IZ": 22687,
+  "Borna IZ": 22688,
+  "Chemnitz IZ": 22689,
+  "Dresden IZ": 22664,
+  "Eich IZ": 22690,
+  "Erz IZ": 22691,
+  "Leipzig Messe IZ": 22692,
+  "Löbau IZ": 22693,
+  "Mittweida IZ": 22694,
+  "Pirna IZ": 22695,
+  "Riesa IZ": 22696,
+  "Zwickau IZ": 22697,
+};
+
+// TESTING
+const NOTIFY_ABOUT_NO_DATES = process.argv
+  .slice(2)
+  .includes("--notify-no-dates");
 
 /**
  * @typedef {Record<string, number>} VaxDates
@@ -222,7 +268,60 @@ async function postChangeLogIfChanged(dates, snapshot, config) {
 }
 
 /**
- * @param {{browser: playwright.Browser, dry: boolean, octokit: Octokit, snapshotPath: string}} config
+ * @param {VaxDates} dates
+ * @param {VaxDates} snapshot
+ * @param {{ mail: Mail }} config
+ * @returns {Promise<void>}
+ */
+async function sendNewsletterIfNewDatesAvailable(dates, snapshot, config) {
+  /**
+   * @type {Set<keyof VaxDates>}
+   */
+  const centresWithNewDates = new Set();
+  Object.keys(dates).forEach((centre) => {
+    const hadNoDates = snapshot[centre] === 0 || snapshot[centre] === undefined;
+    const hasDates = dates[centre] > 0;
+    if (hadNoDates && hasDates) {
+      centresWithNewDates.add(centre);
+    }
+  });
+
+  const sendNotificationsFor = NOTIFY_ABOUT_NO_DATES
+    ? new Set(Object.keys(dates))
+    : centresWithNewDates;
+
+  const results = await Promise.allSettled(
+    Array.from(sendNotificationsFor, async (centre) => {
+      try {
+        await config.mail.sendFreeDates(centre);
+      } catch (error) {
+        error.message = `${centre}: ${error.message}`;
+        throw error;
+      }
+    })
+  );
+
+  const rejections = results.filter(
+    /**
+     * @returns {result is PromiseRejectedResult}
+     */
+    (result) => {
+      return result.status === "rejected";
+    }
+  );
+  if (rejections.length > 0) {
+    throw new Error(
+      `At least one notifications failed:${rejections
+        .map((rejection) => {
+          return `  ${rejection.reason.stack}`;
+        })
+        .join("\n")}`
+    );
+  }
+}
+
+/**
+ * @param {{browser: playwright.Browser, dry: boolean, mail: Mail, octokit: Octokit, snapshotPath: string}} config
  * @returns {Promise<void>}
  */
 async function updateFreeDates(config) {
@@ -231,22 +330,122 @@ async function updateFreeDates(config) {
     loadFreeDatesSnapshot(),
   ]);
 
-  await Promise.all([
+  await saveFreeDates(freeDates, config);
+
+  const results = await Promise.allSettled([
     updateSummary(freeDates, config),
     postChangeLogIfChanged(freeDates, snapshot, config),
+    sendNewsletterIfNewDatesAvailable(freeDates, snapshot, config),
   ]);
 
-  await saveFreeDates(freeDates, config);
+  const rejections = results.filter(
+    /**
+     * @returns {result is PromiseRejectedResult}
+     */
+    (result) => {
+      return result.status === "rejected";
+    }
+  );
+  if (rejections.length > 0) {
+    throw new Error(
+      `At least one update failed:${rejections
+        .map((rejection) => {
+          return `  ${rejection.reason}\n  ${rejection.reason.stack}`;
+        })
+        .join("\n")}`
+    );
+  }
+}
+
+/**
+ * @returns {Mail}
+ */
+function setupSendGrid() {
+  /**
+   * @param {string} endpoint
+   * @param {{method: RequestInit['method'], body: any}} init
+   * @returns {Promise<any>}
+   */
+  async function sgGridRequest(endpoint, init) {
+    if (SENDGRID_API_KEY === undefined) {
+      throw new TypeError(
+        "Need process.env.SENDGRID_API_KEY in order to send mails."
+      );
+    }
+    const request = new Request(
+      new URL(`/v3/${endpoint}`, "https://api.sendgrid.com").toString(),
+      {
+        body: JSON.stringify(init.body),
+        headers: {
+          Authorization: `Bearer ${SENDGRID_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        method: init.method,
+      }
+    );
+    const response = await fetch(request);
+
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(
+      `${request.url}: ${response.status} ${response.statusText}`
+    );
+  }
+
+  return {
+    async sendFreeDates(centre) {
+      const supressionGroupID = CENTRE_TO_SUPRESSION_GROUP_ID[centre];
+      if (supressionGroupID === undefined) {
+        throw new TypeError(
+          `Unable to find supression group ID for centre '${centre}'. Did you mean one of ${Object.keys(
+            CENTRE_TO_SUPRESSION_GROUP_ID
+          )}?`
+        );
+      }
+
+      const singleSend = await sgGridRequest("/marketing/singlesends", {
+        method: "POST",
+        body: {
+          name: `new dates ${centre}`,
+          send_to: {
+            list_ids: [LIST_ID],
+          },
+          email_config: {
+            subject: `${centre}: neue Covid-19 Impftermine`,
+            html_content: "Hello, Dave!",
+            suppression_group_id: supressionGroupID,
+            sender_id: SENDER_ID,
+          },
+        },
+      });
+      console.log(`Created singlesend ${JSON.stringify(singleSend, null, 2)}`);
+
+      const sendAt = new Date(new Date().valueOf() + 1000 * 60).toISOString();
+      console.log(`scheduling for ${sendAt}`);
+      const scheduled = await sgGridRequest(
+        `marketing/singlesends/${singleSend.id}/schedule`,
+        {
+          method: "PUT",
+          body: {
+            send_at: sendAt,
+          },
+        }
+      );
+      console.log(`scheduled for ${JSON.stringify(scheduled, null, 2)}`);
+    },
+  };
 }
 
 async function setup() {
   const browserLaunch = playwright.chromium.launch();
 
   const [browser] = await Promise.all([browserLaunch]);
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
   return {
     browser,
+    sgGrid: setupSendGrid(),
     octokit,
     teardown: async () => {
       await Promise.all([browser.close()]);
@@ -255,7 +454,7 @@ async function setup() {
 }
 
 async function main() {
-  const { browser, octokit, teardown } = await setup();
+  const { browser, octokit, sgGrid, teardown } = await setup();
 
   const dry = process.argv.slice(2).includes("--dry");
   const snapshotPath = new URL("../data/freeDates.json", import.meta.url)
@@ -265,6 +464,7 @@ async function main() {
     await updateFreeDates({
       browser,
       dry,
+      mail: dry ? dryMail : sgGrid,
       octokit,
       snapshotPath,
     });
